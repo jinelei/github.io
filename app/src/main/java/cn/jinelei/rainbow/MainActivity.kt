034c946494bb11b454396994e482a17686ec4bef
= <zhenlei@cqkct.com>
2019-05-27 12:25:40 +0800

    添加tab处理

--- app/src/main/java/cn/jinelei/rainbow/MainActivity.kt
+++ app/src/main/java/cn/jinelei/rainbow/MainActivity.kt
@@ -1,6 +1,7 @@
 package cn.jinelei.rainbow
 
 import android.os.Bundle
+import android.support.design.widget.BottomNavigationView
 import android.support.v7.app.AppCompatActivity
 import android.util.Log
 import com.amap.api.maps.MapView
@@ -12,7 +13,17 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        mMapView = findViewById<MapView>(R.id.map)
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottomNavigationView)
+        bottomNavigationView.setOnNavigationItemSelectedListener { menuItem ->
+                when (menuItem.itemId) {
+                    R.id.navigation_home -> Log.d(TAG, "navigation_home")
+                    R.id.navigation_user -> Log.d(TAG, "navigation_user")
+                    R.id.navigation_discovery -> Log.d(TAG, "navigation_discovery")
+                    else -> Log.d(TAG, "other")
+                }
+                true
+        }
+//        mMapView = findViewById<MapView>(R.id.map)
         mMapView?.onCreate(savedInstanceState)
         val map = mMapView?.map
         Log.d(TAG, map.toString())
