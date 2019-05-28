b8967f31773ac8bf4d8535574119637f68fcdb19
= <zhenlei@cqkct.com>
2019-05-28 18:02:14 +0800

    临时保存recyclerView

--- app/src/main/java/cn/jinelei/rainbow/fragment/UserFragment.kt
+++ app/src/main/java/cn/jinelei/rainbow/fragment/UserFragment.kt
@@ -3,20 +3,38 @@ package cn.jinelei.rainbow.fragment
 import android.content.Intent
 import android.os.Bundle
 import android.support.v4.app.Fragment
+import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
+import android.widget.TextView
 import cn.jinelei.rainbow.R
 import cn.jinelei.rainbow.activity.SettingActivity
+import cn.jinelei.rainbow.util.isFastClick
 
 class UserFragment : Fragment() {
     val TAG = javaClass.simpleName
+    var recyclerView: RecyclerView? = null
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         val view = inflater.inflate(R.layout.user_fragment, container, false)
         val menuBtn = view.findViewById<ImageView>(R.id.menu_setting)
-        menuBtn.setOnClickListener { v: View? -> startActivity(Intent(activity, SettingActivity::class.java)) }
+        menuBtn.setOnClickListener { v: View? ->
+            if (!isFastClick(v))
+                startActivity(Intent(activity, SettingActivity::class.java))
+        }
         return view
+    }
+
+    fun initView(view: View) {
+        recyclerView = view.findViewById<RecyclerView>(R.id.menu_recycler_view)
+        recyclerView?.setHasFixedSize(true)
+        var layoutManager = LinearLayoutManager(activity)
+        recyclerView?.layoutManager = layoutManager
+        var listAdapter = ListMenuViewAdapter(myDataset)
+        recyclerView?.adapter = listAdapter
     }
 
     companion object {
@@ -27,4 +45,32 @@ class UserFragment : Fragment() {
     private object SingletonHolder {
         val holder = UserFragment()
     }
+}
+
+class ListMenuItem(var callback: View.OnClickListener, var title: String, var resId: Int)
+
+
+class ListMenuViewAdapter(var data: List<ListMenuItem>) :
+    RecyclerView.Adapter<ListMenuViewAdapter.ListMenuItemViewHolder>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ListMenuItemViewHolder {
+        var view = LayoutInflater.from(parent.context).inflate(R.layout.list_menu_layout, parent, false)
+        return ListMenuItemViewHolder(view)
+    }
+
+    override fun getItemCount(): Int {
+        return data.size
+    }
+
+    override fun onBindViewHolder(holder: ListMenuItemViewHolder, pos: Int) {
+        val menu = data[pos]
+        holder.icon.setImageResource(menu.resId)
+        holder.title.text = menu.title
+        holder.view.setOnClickListener(menu.callback)
+    }
+
+    class ListMenuItemViewHolder(val view: View) : RecyclerView.ViewHolder(view) {
+        val icon = view.findViewById<ImageView>(R.id.list_menu_icon)
+        val title = view.findViewById<TextView>(R.id.list_menu_title)
+    }
+
 }
---
+++ app/src/main/java/cn/jinelei/rainbow/util/ClickUtils.kt
@@ -0,0 +1,18 @@
+package cn.jinelei.rainbow.util
+
+import android.view.View
+
+
+const val MIN_DELAY_TIME = 100
+val map: MutableMap<View?, Long> = mutableMapOf()
+
+fun isFastClick(view: View?): Boolean {
+    val lastClickTime:Long = map[view] ?: 0L
+    val currentClickTime = System.currentTimeMillis()
+    var result = true
+    if ((currentClickTime - lastClickTime) >= MIN_DELAY_TIME) {
+        result = false
+    }
+    map[view] = currentClickTime
+    return result
+}
