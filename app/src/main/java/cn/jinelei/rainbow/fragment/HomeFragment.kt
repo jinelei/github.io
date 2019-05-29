5716e587ff066fbedabecc3779e8ad606b8eeaf0
= <zhenlei@cqkct.com>
2019-05-29 17:10:51 +0800

    添加滚动监听

--- app/src/main/java/cn/jinelei/rainbow/fragment/HomeFragment.kt
+++ app/src/main/java/cn/jinelei/rainbow/fragment/HomeFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.support.v4.app.FragmentPagerAdapter
 import android.support.v4.view.ViewPager
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -35,6 +36,19 @@ class HomeFragment : BaseFragment() {
 
     fun initView(view: View) {
         val homeViewPager = view.findViewById<ViewPager>(R.id.home_viewpager)
+        homeViewPager.addOnPageChangeListener(object : ViewPager.OnPageChangeListener {
+            override fun onPageSelected(p0: Int) {
+                Log.v(TAG, "onPageSelected $p0")
+            }
+
+            override fun onPageScrollStateChanged(p0: Int) {
+                Log.v(TAG, "onPageScrollStateChanged $p0")
+            }
+
+            override fun onPageScrolled(p0: Int, p1: Float, p2: Int) {
+                Log.v(TAG, "onPageScrolled $p0 $p1 $p2")
+            }
+        })
         homeViewPager.adapter = object : FragmentPagerAdapter(fragmentManager) {
             override fun getCount(): Int {
                 return fragments.size
