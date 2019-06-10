f469b18c6159b7993c935676603e24584aadd8b5
= <zhenlei@cqkct.com>
2019-06-10 13:53:46 +0800

    完善切换语言

--- app/src/main/java/cn/jinelei/rainbow/fragment/UserFragment.kt
+++ app/src/main/java/cn/jinelei/rainbow/fragment/UserFragment.kt
@@ -21,6 +21,7 @@ import cn.jinelei.rainbow.activity.*
 import cn.jinelei.rainbow.application.BaseApplication
 import cn.jinelei.rainbow.util.SharedPreUtil
 import java.util.*
+import kotlin.collections.ArrayList
 
 
 class UserFragment : BaseFragment() {
@@ -32,59 +33,8 @@ class UserFragment : BaseFragment() {
     var navigationTitle: TextView? = null
     var userHeaderIcon: ImageView? = null
     var userHeaderInfo: TextView? = null
-    val listMenu = arrayListOf(
-        ListMenuItem(
-            View.OnClickListener {
-                if (!isFastClick(it)) startActivity(
-                    Intent(
-                        context,
-                        ChangeLanguageActivity::class.java
-                    )
-                )
-            }, "切换语言",
-//            resources.getString(R.string.change_language),
-            R.mipmap.ic_language
-        ),
-        ListMenuItem(
-            View.OnClickListener { if (!isFastClick(it)) startActivity(Intent(context, SetupActivity::class.java)) },
-            "首选项",
-//            resources.getString(R.string.preference),
-            R.mipmap.ic_setup
-        )
-    )
-    val gridMenu = arrayListOf(
-        ListMenuItem(
-            View.OnClickListener { _ -> (activity as BaseActivity).debug(Log.VERBOSE, "verbose") },
-            "verbose",
-            R.mipmap.ic_test
-        ),
-        ListMenuItem(
-            View.OnClickListener { _ -> (activity as BaseActivity).debug(Log.DEBUG, "debug") },
-            "debug",
-            R.mipmap.ic_test
-        ),
-        ListMenuItem(
-            View.OnClickListener { _ -> (activity as BaseActivity).debug(Log.INFO, "info") },
-            "info",
-            R.mipmap.ic_test
-        ),
-        ListMenuItem(
-            View.OnClickListener { _ -> (activity as BaseActivity).debug(Log.WARN, "warn") },
-            "warn",
-            R.mipmap.ic_test
-        ),
-        ListMenuItem(
-            View.OnClickListener { _ -> (activity as BaseActivity).debug(Log.ERROR, "error") },
-            "error",
-            R.mipmap.ic_test
-        ),
-        ListMenuItem(
-            View.OnClickListener {
-                if (!isFastClick(it)) startActivity(Intent(activity, DeviceScanActivity::class.java))
-            },
-            "首选项", R.mipmap.ic_add
-        )
-    )
+    var listMenu: ArrayList<ListMenuItem>? = null
+    var gridMenu: ArrayList<ListMenuItem>? = null
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -92,24 +42,27 @@ class UserFragment : BaseFragment() {
         savedInstanceState: Bundle?
     ): View? {
         return inflater.inflate(R.layout.user_fragment, container, false).apply {
+            initData()
             initView(this)
         }
     }
 
     private fun initView(view: View) {
-        listRecyclerView = view.findViewById<RecyclerView>(R.id.list_menu_recycler_view).apply {
-            setHasFixedSize(true)
-            isNestedScrollingEnabled = false
-            addItemDecoration(DividerItemDecoration(activity, DividerItemDecoration.VERTICAL));
-            layoutManager = LinearLayoutManager(activity)
-            adapter = ListMenuViewAdapter(listMenu)
-        }
-        gridRecyclerView = view.findViewById<RecyclerView>(R.id.grid_menu_recycler_view).apply {
-            setHasFixedSize(true)
-            isNestedScrollingEnabled = false
-            layoutManager = GridLayoutManager(activity, 5)
-            adapter = GridMenuViewAdapter(gridMenu)
-        }
+        if (listMenu != null && listMenu?.size != 0)
+            listRecyclerView = view.findViewById<RecyclerView>(R.id.list_menu_recycler_view).apply {
+                setHasFixedSize(true)
+                isNestedScrollingEnabled = false
+                addItemDecoration(DividerItemDecoration(activity, DividerItemDecoration.VERTICAL));
+                layoutManager = LinearLayoutManager(activity)
+                adapter = ListMenuViewAdapter(listMenu!!)
+            }
+        if (gridMenu != null && gridMenu?.size != 0)
+            gridRecyclerView = view.findViewById<RecyclerView>(R.id.grid_menu_recycler_view).apply {
+                setHasFixedSize(true)
+                isNestedScrollingEnabled = false
+                layoutManager = GridLayoutManager(activity, 5)
+                adapter = GridMenuViewAdapter(gridMenu!!)
+            }
         rightBtn = view.findViewById<ImageView>(R.id.navigation_header_right).apply {
             setImageResource(R.mipmap.ic_add)
             setOnClickListener { if (!isFastClick(it)) startActivity(Intent(activity, DeviceScanActivity::class.java)) }
@@ -127,9 +80,71 @@ class UserFragment : BaseFragment() {
         }
     }
 
+    private fun initData() {
+        listMenu = arrayListOf(
+            ListMenuItem(
+                View.OnClickListener {
+                    if (!isFastClick(it)) startActivity(
+                        Intent(
+                            context,
+                            ChangeLanguageActivity::class.java
+                        )
+                    )
+                },
+                resources.getString(R.string.change_language),
+                R.mipmap.ic_language
+            ),
+            ListMenuItem(
+                View.OnClickListener {
+                    if (!isFastClick(it)) startActivity(
+                        Intent(
+                            context,
+                            SetupActivity::class.java
+                        )
+                    )
+                },
+                resources.getString(R.string.preference),
+                R.mipmap.ic_setup
+            )
+        )
+        gridMenu = arrayListOf(
+            ListMenuItem(
+                View.OnClickListener { _ -> (activity as BaseActivity).debug(Log.VERBOSE, "verbose") },
+                "verbose",
+                R.mipmap.ic_test
+            ),
+            ListMenuItem(
+                View.OnClickListener { _ -> (activity as BaseActivity).debug(Log.DEBUG, "debug") },
+                "debug",
+                R.mipmap.ic_test
+            ),
+            ListMenuItem(
+                View.OnClickListener { _ -> (activity as BaseActivity).debug(Log.INFO, "info") },
+                "info",
+                R.mipmap.ic_test
+            ),
+            ListMenuItem(
+                View.OnClickListener { _ -> (activity as BaseActivity).debug(Log.WARN, "warn") },
+                "warn",
+                R.mipmap.ic_test
+            ),
+            ListMenuItem(
+                View.OnClickListener { _ -> (activity as BaseActivity).debug(Log.ERROR, "error") },
+                "error",
+                R.mipmap.ic_test
+            ),
+            ListMenuItem(
+                View.OnClickListener {
+                    if (!isFastClick(it)) startActivity(Intent(activity, DeviceScanActivity::class.java))
+                },
+                resources.getString(R.string.preference), R.mipmap.ic_add
+            )
+        )
+    }
+
     class ListMenuItem(var callback: View.OnClickListener, var title: String, var resourceId: Int)
 
-    class ListMenuViewAdapter(var data: List<ListMenuItem>) :
+    class ListMenuViewAdapter(var data: ArrayList<ListMenuItem>) :
         RecyclerView.Adapter<ListMenuViewAdapter.ListMenuItemViewHolder>() {
         override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ListMenuItemViewHolder {
             var view = LayoutInflater.from(parent.context).inflate(R.layout.list_menu_layout, parent, false)
@@ -154,7 +169,7 @@ class UserFragment : BaseFragment() {
 
     }
 
-    class GridMenuViewAdapter(var data: List<ListMenuItem>) :
+    class GridMenuViewAdapter(var data: ArrayList<ListMenuItem>) :
         RecyclerView.Adapter<GridMenuViewAdapter.GridMenuItemViewHolder>() {
         override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): GridMenuItemViewHolder {
             return GridMenuItemViewHolder(
